4dee6d398401ce0ecf0d5aae7535ba2371ac9738
Stefan Thomas <moon@justmoon.de>
2009-09-23 22:06:57 +0100

    Database framework now creates it's own database connection.

--- inc.php
+++ inc.php
@@ -3,4 +3,6 @@ $dir = dirname(__FILE__);
 require_once($dir.'/db.class.php');
 require_once($dir.'/obj.class.php');
 require_once($dir.'/set.class.php');
+
+DB::connectMysqli($qconfig['db_user'], $qconfig['db_password'], $qconfig['db_host'], $qconfig['db_name']);
 ?>
